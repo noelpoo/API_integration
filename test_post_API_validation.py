73e205e1d359d39bae91f75f79e4c53660bb3c47
Noel Poo <poonsk@garena.com>
2020-11-19 22:59:06 +0800

    fixed assertion error

--- test_post_API_validation.py
+++ test_post_API_validation.py
@@ -2,9 +2,9 @@ from Common import *
 from serverapi import ServerApi
 from util import generate_random_string
 
-invalid_email = '{}.com@domain'.format(generate_random_string(3))
-short_desc = generate_random_string(49)
-long_desc = generate_random_string(201)
+invalid_email = '{}'.format(generate_random_string(3))
+short_desc = generate_random_string(40)
+long_desc = generate_random_string(500)
 
 
 def test_post_task_missing_field():
@@ -16,7 +16,7 @@ def test_post_task_missing_field():
     resp = client.post_tasks_missing_field(assignee=assignee_email,
                                            reporter=reporter_email,
                                            description=description)
-    assert resp != 200
+    assert resp.status_code != 200
 
 
 def test_post_task_invalid_assignee():
@@ -30,7 +30,7 @@ def test_post_task_invalid_assignee():
                              reporter=reporter_email,
                              description=description,
                              duedate=duedate)
-    assert resp != 200
+    assert resp.status_code != 200
 
 
 def test_post_task_invalid_reporter():
@@ -44,32 +44,29 @@ def test_post_task_invalid_reporter():
                              reporter=reporter_email,
                              description=description,
                              duedate=duedate)
-    assert resp != 200
+    assert resp.status_code != 200
 
 
 def test_post_task_short_description():
     assignee_email = '{}@nodomain.com'.format(generate_random_string(3))
     reporter_email = '{}@nodomain.com'.format(generate_random_string(3))
-    description = short_desc
     duedate = DUEDATE
     client = ServerApi(API_URL)
 
     resp = client.post_tasks(assignee=assignee_email,
                              reporter=reporter_email,
-                             description=description,
+                             description=short_desc,
                              duedate=duedate)
-    assert resp != 200
+    assert resp.status_code != 200
 
 
 def test_post_task_long_description():
     assignee_email = '{}@nodomain.com'.format(generate_random_string(3))
     reporter_email = '{}@nodomain.com'.format(generate_random_string(3))
-    description = long_desc
     duedate = DUEDATE
     client = ServerApi(API_URL)
-
     resp = client.post_tasks(assignee=assignee_email,
                              reporter=reporter_email,
-                             description=description,
+                             description=long_desc,
                              duedate=duedate)
-    assert resp != 200
+    assert resp.status_code != 200
